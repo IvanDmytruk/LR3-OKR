103396e01cbbe90624e3b3c48c198a4708b89ab8
yarty777 <yaroslav0908l@gmail.com>
2025-11-15 22:26:06 +0200

    Fixed images

--- LW3_OKR/LW3_OKR/FVivePersonal.Designer.cs
+++ LW3_OKR/LW3_OKR/FVivePersonal.Designer.cs
@@ -91,9 +91,10 @@
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(23, 30);
+            pictureBox1.Location = new Point(68, 13);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(196, 186);
+            pictureBox1.Size = new Size(134, 213);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
